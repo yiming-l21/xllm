473a077ea8641efd9ddbe65a4fd72876320d8374
yiming-l21 <liuyiming54@jd.com>
2025-09-05 21:06:49 +0800

    feat: init flux execution engine

---
+++ xllm/core/framework/model/causal_flux.h
@@ -0,0 +1,78 @@
+/* Copyright 2025 The xLLM Authors. All Rights Reserved.
+Copyright 2024 The ScaleLLM Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://github.com/jd-opensource/xllm/blob/main/LICENSE
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#pragma once
+
+#include <c10/core/Device.h>
+#include <torch/torch.h>
+
+#include <vector>
+
+#include "core/framework/kv_cache/kv_cache.h"
+#include "core/framework/model_loader.h"
+#include "core/framework/parallel_state.h"
+#include "core/framework/quant_args.h"
+#include "core/framework/state_dict/state_dict.h"
+#include "model_args.h"
+#include "model_input_params.h"
+
+namespace xllm {
+
+class CausalFLUX : public torch::nn::Module {
+ public:
+  ~CausalFLUX() override = default;
+
+  // tokens: [num_tokens]
+  // positions: [num_tokens]
+  // returns: [num_tokens, hidden_size]
+  virtual torch::Tensor forward(const torch::Tensor& tokens,
+                                const torch::Tensor& positions,
+                                std::vector<KVCache>& kv_caches,
+                                const ModelInputParams& parameters) = 0;
+  virtual void load_model(std::unique_ptr<ModelLoader> loader) = 0;
+
+  virtual torch::Device device() const = 0;
+
+  virtual const torch::TensorOptions& options() const = 0;
+};
+
+template <typename Model>
+class CausalFLUXImpl : public CausalFLUX {
+ public:
+  CausalFLUXImpl(Model model, const torch::TensorOptions& options)
+      : model_(std::move(model)), options_(options) {}
+
+  torch::Tensor forward(const torch::Tensor& tokens,
+                        const torch::Tensor& positions,
+                        std::vector<KVCache>& kv_caches,
+                        const ModelInputParams& parameters) override {
+    return model_->forward(tokens, positions, kv_caches, parameters);
+  }
+  void load_model(std::unique_ptr<ModelLoader> loader) override {
+    model_->load_model(std::move(loader));
+  }
+
+  torch::Device device() const override { return options_.device(); }
+
+  const torch::TensorOptions& options() const override { return options_; }
+
+ private:
+  Model model_;
+
+  torch::TensorOptions options_;
+};
+
+}  // namespace xllm
